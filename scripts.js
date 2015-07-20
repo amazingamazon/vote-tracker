1a6684ddf40e63b349bec28a04d671445c641741
Heidi Lowry <heidi.lowry@gmail.com>
2015-07-20 15:45:17 -0700

    added Photo constructor function

--- scripts.js
+++ scripts.js
@@ -32,8 +32,68 @@ var count = 0;
 var vote = document.getElementById("pic1");
 vote.addEventListener("click", function() {
   count += 1;
-  console.log(count);
 });
+
+
+
+var Photo = function(path) {
+  this.path = "img/" + path + ".jpg";
+  this.votes = 0;
+  this.receiveVote = function() {
+    this.votes += 1;
+    console.log(this.votes);
+  };
+};
+
+var cat1 = new Photo("01");
+var cat2 = new Photo("02");
+var cat3 = new Photo("03");
+var cat4 = new Photo("04");
+var cat5 = new Photo("05");
+var cat6 = new Photo("06");
+var cat7 = new Photo("07");
+var cat8 = new Photo("08");
+var cat9 = new Photo("09");
+var cat10 = new Photo("10");
+var cat11 = new Photo("11");
+var cat12 = new Photo("12");
+var cat13 = new Photo("13");
+var cat14 = new Photo("14");
+
+var cats = [];
+cats.push(cat1, cat2, cat3, cat4, cat5, cat6, cat7, cat8, cat9, cat10, cat11, cat12, cat13, cat14);
+
+console.log(cat1.path);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
