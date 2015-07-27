16e95535824c88d0538034bd7d397b12f48e00f7
Heidi Lowry <heidi.lowry@gmail.com>
2015-07-26 22:41:07 -0700

    set up node.js server

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var http = require('http');
+var app = express();
+
+app.use(express.static('public'));
+
+app.get('/secret', function(res, req){
+  res.status(200).send('THE fog comes</br>on little cat feet.</br></br>It sits looking</br>over harbor and city</br>on silent haunches</br>and then moves on.</br></br><b>Carl Sandburg</b>, <em>Fog</em>');
+});
+
+app.get('/*', function(res, req) {
+  res.status(404).sendFile('public/404.html');
+});
+
+http.createServer(app).listen(5000);
+console.log("The server is listening on port 5000...");
