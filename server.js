c5d75f4812880440f21a3fc3c874eb5d697f4faa
Heidi Lowry <heidi.lowry@gmail.com>
2015-07-27 20:05:46 -0700

    added 404 html file

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
 var express = require('express');
 var http = require('http');
 var app = express();
+var path = require('path');
 
 app.use(express.static('public'));
 
 app.get('/secret', function(req, res) {
-  // res.status(200).send('THE fog comes</br>on little cat feet.</br></br>It sits looking</br>over harbor and city</br>on silent haunches</br>and then moves on.</br></br><b>Carl Sandburg</b>, <em>Fog</em>');
-  console.log("getting secret...");
-  res.send("This is my secret stash!");
+  res.status(200).send('THE fog comes</br>on little cat feet.</br></br>It sits looking</br>over harbor and city</br>on silent haunches</br>and then moves on.</br></br><b>Carl Sandburg</b>, <em>Fog</em>');
 });
 
 app.get('/*', function(req, res) {
-  // res.status(404).sendFile('public/404.html');
-  res.status(404).send("404: Nothing to see here.");
+  res.sendFile(path.join(__dirname, '/public/404.html'));
 });
 
 http.createServer(app).listen(5000);
